ff5c00ffad089365654999c04a8c3ac1b1ec7da3
vfdeoliveira <vinicius.o@aln.senaicimatec.edu.br>
2025-03-16 22:35:26 -0300

    Adicionando função para Verificação Paralela

---
+++ src/Verificador/VerficadorPrimosParalelo.java
@@ -0,0 +1,40 @@
+package Verificador;
+import Utils.Utils;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VerficadorPrimosParalelo extends Object {
+
+    public List<Integer> numeros;
+    public List<Integer> primos = new ArrayList<>();
+    private int currentIndex = 0;
+
+    public VerficadorPrimosParalelo(List<Integer> numeros) throws Exception {
+        this.numeros = numeros;
+    }
+
+    public void loopThread() {
+        int numerosAnalisados = 0;
+        while (true) {
+            try {
+                verificarProximoNumero();
+                numerosAnalisados++;
+            } catch (Exception e) {
+                break;
+            }
+        }
+        System.out.println("Thread " + Thread.currentThread().getName() + " analisou " + numerosAnalisados + " números.");
+    }
+
+    public synchronized void verificarProximoNumero() throws Exception {
+        try {
+            //System.out.println("Thread " + Thread.currentThread().getName() + " verificou o número " + numeros.get(currentIndex));
+            if (Utils.ehPrimo(numeros.get(currentIndex))) {
+                primos.add(numeros.get(currentIndex));
+            }
+            currentIndex++;
+        } catch (Exception e) {
+            throw new Exception("Todos os números analisados");
+        }
+    }
+}
